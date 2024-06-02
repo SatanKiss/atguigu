9251136d6b1a191b712ece7c49c986dab8029a7b
SatanKiss <2036736521@qq.com>
2024-06-02 23:32:47 +0800

    P22 END

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 
+import com.atguigu.gulimall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,16 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity member = new MemberEntity();
+        member.setNickname("SatanKiss");
+        R membercoupons = couponFeignService.membercoupons();
+        return R.ok().put("member",member).put("coupons",membercoupons.get("coupons"));
+    }
     /**
      * 列表
      */
